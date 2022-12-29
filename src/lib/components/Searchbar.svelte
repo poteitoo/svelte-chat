8cac25d6fb4016c31cbfc1d35e3cac4a0b87ee3d
poteitoo <1105tigery@gmail.com>
2022-12-30 03:25:26 +0900

    add header components

---
+++ src/lib/components/Searchbar.svelte
@@ -0,0 +1,35 @@
+<form class="w-full md:w-1/2  py-3">
+	<label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+		>Search</label
+	>
+	<div class="relative">
+		<div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+			<svg
+				aria-hidden="true"
+				class="w-5 h-5 text-gray-500 dark:text-gray-400"
+				fill="none"
+				stroke="currentColor"
+				viewBox="0 0 24 24"
+				xmlns="http://www.w3.org/2000/svg"
+				><path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+				/></svg
+			>
+		</div>
+		<input
+			type="search"
+			id="default-search"
+			class="input input-bordered block w-full pl-10"
+			placeholder="Search Mockups, Logos..."
+			required
+		/>
+		<button
+			type="submit"
+			class="btn btn-sm absolute right-2.5 inset-y-0 h-5 text-sm m-auto hidden md:block"
+			>Search</button
+		>
+	</div>
+</form>
---
+++ src/lib/components/UserButton.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	let authSignLink = $page.data.session ? '/auth/signout' : '/auth/signin';
+	let username = $page.data.session?.user?.email ?? $page.data.session?.user?.name;
+	username = username
+		? username.length > 20
+			? username.substring(0, 20) + '...'
+			: username
+		: null;
+</script>
+
+<a href={authSignLink}>
+	<button class="btn btn-ghost m-3 flex-row gap-3 max-w-64 hidden md:flex">
+		{#if $page.data.session}
+			<div class="w-8 rounded-xl">
+				<img src={$page.data.session.user?.image} alt="" />
+			</div>
+			{username}
+		{:else}
+			<div class="">SignIn</div>
+		{/if}
+	</button>
+</a>
