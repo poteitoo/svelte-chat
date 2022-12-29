fa3034ebd1461c5d8efe3a9770bd2c1555f891bd
poteitoo <1105tigery@gmail.com>
2022-12-30 03:22:57 +0900

    update remove sample code

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,32 +1,5 @@
 <script lang="ts">
 	import '../app.css';
-	import { page } from '$app/stores';
 </script>
-
-<header>
-	<div class="signedInStatus">
-		<p class="nojs-show loaded">
-			{#if $page.data.session}
-				{#if $page.data.session.user?.image}
-					<span style="background-image: url('{$page.data.session.user.image}')" class="avatar" />
-				{/if}
-				<span class="signedInText">
-					<small>Signed in as</small><br />
-					<strong>{$page.data.session.user?.email ?? $page.data.session.user?.name}</strong>
-				</span>
-				<a href="/auth/signout" class="button">Sign out</a>
-			{:else}
-				<span class="notSignedInText">You are not signed in</span>
-				<a href="/auth/signin" class="buttonPrimary">Sign in</a>
-			{/if}
-		</p>
-	</div>
-	<nav>
-		<ul class="navItems">
-			<li class="navItem"><a href="/">Home</a></li>
-			<li class="navItem"><a href="/protected">Protected</a></li>
-		</ul>
-	</nav>
-</header>
 
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { page } from '$app/stores';
+	import UserButton from '$lib/components/UserButton.svelte';
+</script>
+
+<UserButton />
+{#if $page.data.session}
+	<a href="/chat/poepoe" class="">poepoe link</a>
+{/if}
