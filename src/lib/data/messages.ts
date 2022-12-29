dbbc63ecdd88513774db0b7bd79f28472855af59
poteitoo <1105tigery@gmail.com>
2022-12-30 03:25:55 +0900

    add message type & sample data

---
+++ src/lib/data/messages.ts
@@ -0,0 +1,49 @@
+import type { Message } from '../types';
+
+export const templateMessages1: Message[] = [
+	{
+		id: 'awefwf',
+		content: `It's over Anakin,
+I have the high ground.`,
+		ownerId: 'pjwgwegp',
+		chatRoomId: 'pafjpaweg',
+		createdAt: new Date(),
+		updatedAt: new Date()
+	},
+	{
+		id: 'pwepwe',
+		content: `You underestimate my power!`,
+		ownerId: 'poepoe',
+		chatRoomId: 'pafjpaweg',
+		createdAt: new Date(),
+		updatedAt: new Date()
+	},
+	{
+		id: 'awevgawe',
+		content: `Hello world!!`,
+		ownerId: 'poepoe',
+		chatRoomId: 'pafjpaweg',
+		createdAt: new Date(),
+		updatedAt: new Date()
+	}
+];
+
+export const templateMessages2: Message[] = [
+	{
+		id: 'vadasb',
+		content: `It's over Anakin,
+I have the high ground.`,
+		ownerId: 'poepoe',
+		chatRoomId: 'awegaweg',
+		createdAt: new Date(),
+		updatedAt: new Date()
+	},
+	{
+		id: 'wegawea',
+		content: `You underestimate my power!`,
+		ownerId: 'pjwgwegp',
+		chatRoomId: 'aweaewga',
+		createdAt: new Date(),
+		updatedAt: new Date()
+	}
+];
---
+++ src/lib/types/index.ts
@@ -0,0 +1,8 @@
+export type Message = {
+	id: string;
+	content: string;
+	ownerId: string;
+	chatRoomId: string;
+	createdAt: Date;
+	updatedAt: Date;
+};
